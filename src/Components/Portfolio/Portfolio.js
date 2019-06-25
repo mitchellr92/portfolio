e7ce304c018f71447a89e4b0d92aae20812b6c8a
Mitchell Robles <mitchellr92@live.com>
2019-06-25 11:34:51 -0700

    Added some placeholders for projects and some more styling

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,14 +1,44 @@
-import React, { Component } from 'react';
-import './Portfolio.css';
+import React, { Component } from "react";
+import "./Portfolio.css";
 
 class Portfolio extends Component {
-    render() {
-        return (
-            <div>
-                Hello World!
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="portfolio-container">
+        <div className="portfolio">
+          <div className="project">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <div className="project">
+            Ac tortor vitae purus faucibus ornare suspendisse. Faucibus in
+            ornare quam viverra orci sagittis. Pulvinar proin gravida hendrerit
+            lectus. Aliquam vestibulum morbi blandit cursus risus. Vel eros
+            donec ac odio tempor orci dapibus ultrices in. Ultricies tristique
+            nulla aliquet enim tortor. Aliquam ut porttitor leo a diam
+            sollicitudin tempor id eu. Mi eget mauris pharetra et ultrices.
+            Curabitur vitae nunc sed velit dignissim sodales. Sollicitudin nibh
+            sit amet commodo nulla facilisi.
+          </div>
+          <div className="project">
+            Enim eu turpis egestas pretium aenean pharetra magna. Duis tristique
+            sollicitudin nibh sit amet commodo nulla. Odio tempor orci dapibus
+            ultrices in iaculis nunc sed. Eu mi bibendum neque egestas congue
+            quisque egestas. Eget nulla facilisi etiam dignissim. Elementum nibh
+            tellus molestie nunc non blandit massa enim. Mattis aliquam faucibus
+            purus in massa tempor nec. Metus dictum at tempor commodo
+            ullamcorper a. Id aliquet lectus proin nibh nisl condimentum id
+            venenatis a. Pellentesque habitant morbi tristique senectus et.
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Portfolio;
