e154c118d39f33646ee8128014de1ea5828c106e
Mitchell Robles <mitchellr92@live.com>
2019-06-25 10:44:42 -0700

    created basic header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react';
-import './Header.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import 'font-awesome/less/font-awesome.less';
+import "./Header.css";
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                Hello World!
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="header-container">
+        <div className="header">
+          <div className="button">Top</div>
+          <div className="button">About Me</div>
+          <div className="button">Portfolio</div>
+          <div className="button">Contact</div>
+          <a className="button" href="https://github.com/mitchellr92">
+            <i class="fab fa-github" />
+          </a>
+          <a
+            className="button"
+            href="https://www.linkedin.com/in/mitchell-robles-aa7980165/"
+          >
+            <i class="fab fa-linkedin-in" />
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
