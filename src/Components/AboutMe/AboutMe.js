e4b514f54414ab025b14690afac9b3e2d6b84a14
Mitchell Robles <mitchellr92@live.com>
2019-06-25 11:18:03 -0700

    Added about me and some minor styling

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
-import './AboutMe.css';
+import React, { Component } from "react";
+import "./AboutMe.css";
 
 class AboutMe extends Component {
-    render() {
-        return (
-            <div>
-                Hello World!
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="about-container">
+        <h2 className="about-title">About Me</h2>
+        <p className="about-content">
+          Full-Stack Web Developer who is passionate about problem solving and
+          creating quality products. I enjoy working on teams and communicating
+          with others to reach a common goal. When I am not busy with work or
+          study spend most of my time with my family, in the outdoors or
+          watching movies.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default AboutMe;
