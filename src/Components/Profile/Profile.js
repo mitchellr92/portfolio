fc543d019a3000018f98644d5c49c7d2629b70f8
Mitchell Robles <mitchellr92@live.com>
2019-12-18 19:00:12 -0800

    added about me section

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import profilePic from "./download.png";
 import "./Profile.css";
 
 class Profile extends Component {
@@ -18,42 +19,41 @@ class Profile extends Component {
   render() {
     console.log(this.props.contactMe);
     return (
-      <div className="profile-container">
-        <div className="profile">
-          <p className="name">Mitchell Robles</p>
-          <p className="divider">|</p>
-          <p className="title">Web Developer</p>
-        </div>
-        <div className="links">
-          <div className="link-buttons">
-            <a
-              className="button"
-              target="_blank"
-              href="https://github.com/mitchellr92"
-            >
-              <i className="fab fa-github" />
-            </a>
-            <a
-              className="button"
-              target="_blank"
-              href="https://www.linkedin.com/in/mitchell-robles-aa7980165/"
-            >
-              <i className="fab fa-linkedin-in" />
-            </a>
-            <a
-              className="button"
-              target="_blank"
-              href="https://resume.creddle.io/resume/3h5u0hc5mbd"
-            >
-              <i className="fas fa-file"></i>
-            </a>
-            <a className="button" onClick={this.props.contactMe}>
-              <i className="fas fa-envelope"></i>
-            </a>
+      <div className="page-container">
+        <div className="profile-container">
+          <div className="profile">
+            <p className="name">Mitchell Robles</p>
+            <p className="divider">|</p>
+            <p className="title">Web Developer</p>
           </div>
-          <Link className="projects" to={"/portfolio"}>
-            Projects
-          </Link>
+          <div className="links">
+            <div className="link-buttons">
+              <a
+                className="button"
+                target="_blank"
+                href="https://github.com/mitchellr92"
+              >
+                <i className="fab fa-github" />
+              </a>
+              <a
+                className="button"
+                target="_blank"
+                href="https://www.linkedin.com/in/mitchell-robles-aa7980165/"
+              >
+                <i className="fab fa-linkedin-in" />
+              </a>
+              <a className="button" onClick={this.props.contactMe}>
+                <i className="fas fa-envelope"></i>
+              </a>
+            </div>
+            <Link className="projects" to={"/portfolio"}>
+              Projects
+            </Link>
+          </div>
+        </div>
+        <div className="profile-summary">
+          <img src={profilePic} className="profile-pic" />
+          <p>Collaborative Full Stack Web Developer with experience working on multiple distributed cross-functional agile development teams. Built multiple projects using JavaScript, HTML, CSS, and Node.js. 7 years’ experience as an Electrician developing attention to detail, critical thinking, and project management skill set.</p>
         </div>
       </div>
     );
